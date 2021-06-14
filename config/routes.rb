11a063b7fb630f5d20d8dc76eda21bb12a8f306d
vanloi1997 <loidinh174@gmail.com>
2021-06-14 13:45:59 +0700

    fisrt

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+
+    namespace :v2 do
+      resources :members
+      resources :bands 
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
